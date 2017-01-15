811d1acf943a148d7dc1a0f94c7fec6c0261a666
Irina O'Hara <irina.v.ohara@gmail.com>
2017-01-15 13:39:50 -0700

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	post '/home/contact_us' => 'home#index', as: "contact_us"
 	get '/the_crossings' => 'home#the_crossings'
+	get '/amenities' => 'home#amenities'
+	get '/heber_valley' => 'home#heber_valley'
 	resources :posts
   	root 'home#index'
 
